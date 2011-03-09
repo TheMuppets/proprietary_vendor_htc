751c09ba50519895a650678dd0c94db1da9895e3
KalimochoAz <calimochoazucarado@gmail.com>
2011-03-09 17:07:07 +0100

    Click modified libs and removed overlays

--- click/click-vendor.mk
+++ click/click-vendor.mk
@@ -16,17 +16,11 @@
 
 # Live wallpaper packages
 PRODUCT_PACKAGES := \
-    LiveWallpapers \
     LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    VisualizationWallpapers \
     librs_jni
 
 # Publish that we support the live wallpaper feature.
 PRODUCT_COPY_FILES := \
     packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
-
-# Pick up overlay for features that depend on non-open-source files
-DEVICE_PACKAGE_OVERLAYS := vendor/htc/click/overlay
 
 $(call inherit-product, vendor/htc/click/device-vendor-blobs.mk)
