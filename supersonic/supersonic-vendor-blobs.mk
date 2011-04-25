e898c1770fc85477c3dc79a3a539afeb815046c5
Steve Kondik <shade@chemlab.org>
2011-04-25 02:00:27 -0400

    supersonic: Update props

--- supersonic/supersonic-vendor-blobs.mk
+++ supersonic/supersonic-vendor-blobs.mk
@@ -29,7 +29,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/supersonic/proprietary/WPDB.zip:/system/etc/WPDB.zip \
     vendor/htc/supersonic/proprietary/Supersonic_20100204_Sprint_Jack_324016_v2.acdb:/system/etc/firmware/Supersonic_20100204_Sprint_Jack_324016_v2.acdb \
     vendor/htc/supersonic/proprietary/default.acdb:/system/etc/firmware/default.acdb \
-    vendor/htc/supersonic/proprietary/gps.supersonic.so:/system/lib/hw/gps.supersonic.so \
     vendor/htc/supersonic/proprietary/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
     vendor/htc/supersonic/proprietary/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
     vendor/htc/supersonic/proprietary/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
