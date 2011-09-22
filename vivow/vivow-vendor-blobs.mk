0ac8f7a7e1a0e6784e127fc6e223091e5f7ee62d
Giulio Cervera <giulio.cervera@gmail.com>
2011-09-22 22:30:38 +0200

    vivo: vivow: swicth to htc ril

--- vivow/vivow-vendor-blobs.mk
+++ vivow/vivow-vendor-blobs.mk
@@ -17,12 +17,16 @@
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES += \
     vendor/htc/vivow/proprietary/libcamera.so:obj/lib/libcamera.so \
-    vendor/htc/vivow/proprietary/libril.so:obj/lib/libril.so \
-    vendor/htc/vivow/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so
+    vendor/htc/vivo/proprietary/libaudioalsa.so:obj/lib/libaudioalsa.so \
+    vendor/htc/vivo/proprietary/libril.so:obj/lib/libril.so
+
 
 # All the blobs necessary for vivow
 PRODUCT_COPY_FILES += \
     vendor/htc/vivow/proprietary/akmd:/system/bin/akmd \
+    vendor/htc/vivow/proprietary/rild:/system/bin/rild \
+    vendor/htc/vivow/proprietary/qmuxd:/system/bin/qmuxd \
+    vendor/htc/vivow/proprietary/netmgrd:/system/bin/netmgrd \
     vendor/htc/vivow/proprietary/awb_camera:/system/bin/awb_camera \
     vendor/htc/vivow/proprietary/bma150_usr:/system/bin/bma150_usr \
     vendor/htc/vivow/proprietary/dmagent:/system/bin/dmagent \
@@ -41,7 +45,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/vivow/proprietary/libaudioalsa.so:/system/lib/libaudioalsa.so \
     vendor/htc/vivow/proprietary/libcamera.so:/system/lib/libcamera.so \
     vendor/htc/vivow/proprietary/liboemcamera.so:/system/lib/liboemcamera.so \
-    vendor/htc/vivow/proprietary/libposteffect.so:/system/lib/libposteffect.so \
     vendor/htc/vivow/proprietary/libgemini.so:/system/lib/libgemini.so \
     vendor/htc/vivow/proprietary/libmmipl.so:/system/lib/libmmipl.so \
     vendor/htc/vivow/proprietary/libmmjpeg.so:/system/lib/libmmjpeg.so \
@@ -49,4 +52,13 @@ PRODUCT_COPY_FILES += \
     vendor/htc/vivow/proprietary/libims_ril.so:/system/lib/libims_ril.so \
     vendor/htc/vivow/proprietary/libreference-ril.so:/system/lib/libreference-ril.so \
     vendor/htc/vivow/proprietary/libril_ims.so:/system/lib/libreference-ril.so \
-    vendor/htc/vivow/proprietary/libril.so:/system/lib/libril.so
+    vendor/htc/vivow/proprietary/libhtc_ril.so:/system/lib/libhtc_ril.so \
+    vendor/htc/vivow/proprietary/libril.so:system/lib/libril.so \
+    vendor/htc/vivow/proprietary/libdsi_netctrl.so:/system/lib/libdsi_netctrl.so \
+    vendor/htc/vivow/proprietary/libqmi.so:/system/lib/libqmi.so \
+    vendor/htc/vivow/proprietary/libdll.so:/system/lib/libdll.so \
+    vendor/htc/vivow/proprietary/libdiag.so:/system/lib/libdiag.so \
+    vendor/htc/vivow/proprietary/libidl.so:/system/lib/libidl.so \
+    vendor/htc/vivow/proprietary/libnetmgr.so:/system/lib/libnetmgr.so \
+    vendor/htc/vivow/proprietary/libdsutils.so:/system/lib/libdsutils.so \
+    vendor/htc/vivow/proprietary/libqdp.so:/system/lib/libqdp.so
