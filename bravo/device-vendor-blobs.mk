da055b9730c2d2cd48fca556a251a19ff99f5703
Giulio Cervera <giulio.cervera@gmail.com>
2012-07-04 16:19:41 +0200

    Revert "bravo: use HTC ril"
    This reverts commit 6aad09d0a912febde4711071f3a50efb77651519.

--- bravo/device-vendor-blobs.mk
+++ bravo/device-vendor-blobs.mk
@@ -33,13 +33,10 @@ PRODUCT_COPY_FILES += \
     vendor/htc/bravo/proprietary/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/htc/bravo/proprietary/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
     vendor/htc/bravo/proprietary/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-    vendor/htc/bravo/proprietary/librpc.so:system/lib/librpc.so \
     vendor/htc/bravo/proprietary/gps.bravo.so:system/lib/hw/gps.bravo.so \
     vendor/htc/bravo/proprietary/libcamera.so:system/lib/libcamera.so \
     vendor/htc/bravo/proprietary/libgsl.so:system/lib/libgsl.so \
     vendor/htc/bravo/proprietary/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
     vendor/htc/bravo/proprietary/libhtc_ril.so:system/lib/libhtc_ril.so \
-    vendor/htc/bravo/proprietary/libril.so:system/lib/libril.so \
-    vendor/htc/bravo/proprietary/rild:system/bin/rild \
     vendor/htc/bravo/proprietary/liboemcamera.so:system/lib/liboemcamera.so \
     vendor/htc/bravo/proprietary/libOmxVdec.so:system/lib/libOmxVdec.so
