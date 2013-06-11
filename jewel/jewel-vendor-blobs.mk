7378962a01a2aa0578affcabf010f171cbb1fe71
Ethan Chen <intervigil@gmail.com>
2013-06-10 23:29:02 -0700

    jewel: move QMI libraries to device-specific folders
    * CDMA/GSM devices expect unique versions of these libraries
    * HTC version: Sprint 3.15.651.16

--- jewel/jewel-vendor-blobs.mk
+++ jewel/jewel-vendor-blobs.mk
@@ -31,6 +31,7 @@ PRODUCT_COPY_FILES += \
         vendor/htc/jewel/proprietary/lib/libmlplatform.so:/system/lib/libmlplatform.so \
         vendor/htc/jewel/proprietary/lib/libmllite.so:/system/lib/libmllite.so \
         vendor/htc/jewel/proprietary/lib/libmpl.so:/system/lib/libmpl.so \
+        vendor/htc/jewel/proprietary/bin/qmuxd:/system/bin/qmuxd \
         vendor/htc/jewel/proprietary/etc/firmware/modem.b00:/system/etc/firmware/modem.b00 \
         vendor/htc/jewel/proprietary/etc/firmware/modem.b01:/system/etc/firmware/modem.b01 \
         vendor/htc/jewel/proprietary/etc/firmware/modem.b02:/system/etc/firmware/modem.b02 \
@@ -59,5 +60,17 @@ PRODUCT_COPY_FILES += \
         vendor/htc/jewel/proprietary/etc/firmware/modem_fw.b26:/system/etc/firmware/modem_fw.b26 \
         vendor/htc/jewel/proprietary/etc/firmware/modem_fw.b29:/system/etc/firmware/modem_fw.b29 \
         vendor/htc/jewel/proprietary/etc/firmware/modem_fw.mdt:/system/etc/firmware/modem_fw.mdt \
+        vendor/htc/jewel/proprietary/lib/libdiag.so:/system/lib/libdiag.so \
+        vendor/htc/jewel/proprietary/lib/libdsi_netctrl.so:/system/lib/libdsi_netctrl.so \
+        vendor/htc/jewel/proprietary/lib/libdsutils.so:/system/lib/libdsutils.so \
+        vendor/htc/jewel/proprietary/lib/libidl.so:/system/lib/libidl.so \
+        vendor/htc/jewel/proprietary/lib/libqdi.so:/system/lib/libqdi.so \
+        vendor/htc/jewel/proprietary/lib/libqdp.so:/system/lib/libqdp.so \
+        vendor/htc/jewel/proprietary/lib/libqmi_cci.so:/system/lib/libqmi_cci.so \
+        vendor/htc/jewel/proprietary/lib/libqmi_common_so.so:/system/lib/libqmi_common_so.so \
+        vendor/htc/jewel/proprietary/lib/libqmi_csi.so:/system/lib/libqmi_csi.so \
+        vendor/htc/jewel/proprietary/lib/libqmi_encdec.so:/system/lib/libqmi_encdec.so \
+        vendor/htc/jewel/proprietary/lib/libqmiservices.so:/system/lib/libqmiservices.so \
+        vendor/htc/jewel/proprietary/lib/libqmi.so:/system/lib/libqmi.so \
         vendor/htc/jewel/proprietary/lib/libril-qc-qmi-1.so:/system/lib/libril-qc-qmi-1.so \
         vendor/htc/jewel/proprietary/lib/libril-qcril-hook-oem.so:/system/lib/libril-qcril-hook-oem.so
