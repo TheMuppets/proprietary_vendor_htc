b1e0fc62248ef9fd7c4e60dae47ad04a1a023261
Thomas Wendt <thoemy@gmx.net>
2013-08-28 21:10:26 +0200

    endeavoru: Add prebuilt/audio files from device tree
    Change-Id: I3297f23c13b133ba934bbf35be850310fa838131

--- endeavoru/endeavoru-vendor-blobs.mk
+++ endeavoru/endeavoru-vendor-blobs.mk
@@ -123,6 +123,9 @@ PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin \
   vendor/htc/endeavoru/proprietary/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin \
   vendor/htc/endeavoru/proprietary/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin:system/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin \
+  vendor/htc/endeavoru/proprietary/etc/AIC3008_REG_DualMic.csv:system/etc/AIC3008_REG_DualMic.csv \
+  vendor/htc/endeavoru/proprietary/etc/AIC3008_REG_DualMic_XC.csv:system/etc/AIC3008_REG_DualMic_XC.csv \
+  vendor/htc/endeavoru/proprietary/etc/DSP_number.txt:system/etc/DSP_number.txt \
   vendor/htc/endeavoru/proprietary/etc/soundimage/CodecDSPID.txt:system/etc/soundimage/CodecDSPID.txt \
   vendor/htc/endeavoru/proprietary/etc/soundimage/CodecDSPID_XC.txt:system/etc/soundimage/CodecDSPID_XC.txt \
   vendor/htc/endeavoru/proprietary/etc/soundimage/Sound_BT_CarMode.txt:system/etc/soundimage/Sound_BT_CarMode.txt \
