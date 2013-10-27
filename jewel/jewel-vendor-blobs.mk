cd6322773a7640e7f97e3ce856beb4a51b70c958
jared caliendo <kushdeck@gmail.com>
2013-10-27 03:54:48 -0700

    jewel: update radio props to RUU 3.17.
    Change-Id: If4e2ef661f9f23a8bc0910e217af0ab56d9df3d5

--- jewel/jewel-vendor-blobs.mk
+++ jewel/jewel-vendor-blobs.mk
@@ -25,8 +25,10 @@ PRODUCT_COPY_FILES += \
         vendor/htc/jewel/proprietary/lib/libchromatix_s5k6a1gx_zsl.so:/system/lib/libchromatix_s5k6a1gx_zsl.so \
         vendor/htc/jewel/proprietary/lib/hw/nfc.default.so:/system/lib/hw/nfc.default.so \
         vendor/htc/jewel/proprietary/vendor/firmware/libpn544_fw.so:/system/vendor/firmware/libpn544_fw.so \
+        vendor/htc/jewel/proprietary/lib/libhtc_rilhook.so:/system/lib/libhtc_rilhook.so \
         vendor/htc/jewel/proprietary/lib/libqcci_legacy.so:/system/lib/libqcci_legacy.so \
         vendor/htc/jewel/proprietary/lib/libqmi_client_qmux.so:/system/lib/libqmi_client_qmux.so \
+        vendor/htc/jewel/proprietary/lib/libqmi_csvt_srvc.so:/system/lib/libqmi_csvt_srvc.so \
         vendor/htc/jewel/proprietary/lib/hw/sensors.default.so:/system/lib/hw/sensors.default.so \
         vendor/htc/jewel/proprietary/lib/libimu.so:/system/lib/libimu.so \
         vendor/htc/jewel/proprietary/lib/libmlplatform.so:/system/lib/libmlplatform.so \
