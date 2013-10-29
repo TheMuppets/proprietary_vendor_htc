edaa034b6578919d1f4a99a59f057196a3d35a72
Thomas Wendt <thoemy@gmx.net>
2013-10-30 00:31:53 +0100

    endeavoru: Update remaining blobs except graphics related ones
    Change-Id: Ifef3a44a776a1803aad7e8d607645969ea9ec066

--- endeavoru/endeavoru-vendor-blobs.mk
+++ endeavoru/endeavoru-vendor-blobs.mk
@@ -24,9 +24,11 @@
 PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/bin/uim-sysfs:system/bin/uim-sysfs \
   vendor/htc/endeavoru/proprietary/bin/nvm_server:system/bin/nvm_server \
+  vendor/htc/endeavoru/proprietary/bin/nvm_client:system/bin/nvm_client \
   vendor/htc/endeavoru/proprietary/bin/mknod:system/bin/mknod \
   vendor/htc/endeavoru/proprietary/bin/make_sec_bin:system/bin/make_sec_bin \
   vendor/htc/endeavoru/proprietary/bin/IMCdownload:system/bin/IMCdownload \
+  vendor/htc/endeavoru/proprietary/etc/Flash_Loader.conf:system/etc/Flash_Loader.conf \
   vendor/htc/endeavoru/proprietary/bin/InjectionTool:system/bin/InjectionTool \
   vendor/htc/endeavoru/proprietary/bin/rild:system/bin/rild \
   vendor/htc/endeavoru/proprietary/bin/gsm0710muxd:system/bin/gsm0710muxd \
@@ -34,14 +36,20 @@ PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/bin/poweron_modem_fls.sh:system/bin/poweron_modem_fls.sh \
   vendor/htc/endeavoru/proprietary/lib/libhtc_ril.so:system/lib/libhtc_ril.so \
   vendor/htc/endeavoru/proprietary/lib/libhtc_rilhook.so:system/lib/libhtc_rilhook.so \
+  vendor/htc/endeavoru/proprietary/lib/libril-icera.so:system/lib/libril-icera.so \
+  vendor/htc/endeavoru/proprietary/lib/libril.so:system/lib/libril.so \
   vendor/htc/endeavoru/proprietary/bin/initial_regdom.sh:system/bin/initial_regdom.sh \
   vendor/htc/endeavoru/proprietary/bin/iw:system/bin/iw \
   vendor/htc/endeavoru/proprietary/bin/l2_profile_exchange:system/bin/l2_profile_exchange \
   vendor/htc/endeavoru/proprietary/bin/hdmid:system/bin/hdmid \
   vendor/htc/endeavoru/proprietary/lib/libmllite.so:system/lib/libmllite.so \
   vendor/htc/endeavoru/proprietary/lib/libinvensense_hal.so:system/lib/libinvensense_hal.so \
-  vendor/htc/endeavoru/proprietary/bin/nvcpud:system/bin/nvcpud \
+  vendor/htc/endeavoru/proprietary/bin/nvcap_test:system/bin/nvcap_test \
+  vendor/htc/endeavoru/proprietary/bin/nvtest:system/bin/nvtest \
+  vendor/htc/endeavoru/proprietary/bin/nv_hciattach:system/bin/nv_hciattach \
+  vendor/htc/endeavoru/proprietary/bin/getnvm.sh:system/bin/getnvm.sh \
   vendor/htc/endeavoru/proprietary/bin/tf_daemon:system/bin/tf_daemon \
+  vendor/htc/endeavoru/proprietary/bin/tegrastats:system/bin/tegrastats \
   vendor/htc/endeavoru/proprietary/lib/libcgdrv.so:system/lib/libcgdrv.so \
   vendor/htc/endeavoru/proprietary/lib/libardrv_dynamic.so:system/lib/libardrv_dynamic.so \
   vendor/htc/endeavoru/proprietary/lib/libnvapputil.so:system/lib/libnvapputil.so \
@@ -54,8 +62,8 @@ PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/lib/libnvcapclk.so:system/lib/libnvcapclk.so \
   vendor/htc/endeavoru/proprietary/lib/libnvcap.so:system/lib/libnvcap.so \
   vendor/htc/endeavoru/proprietary/lib/libnvcap_video.so:system/lib/libnvcap_video.so \
+  vendor/htc/endeavoru/proprietary/lib/libnvcpl.so:system/lib/libnvcpl.so \
   vendor/htc/endeavoru/proprietary/lib/libnvcontrol_jni.so:system/lib/libnvcontrol_jni.so \
-  vendor/htc/endeavoru/proprietary/lib/libnvcpud.so:system/lib/libnvcpud.so \
   vendor/htc/endeavoru/proprietary/lib/libnvddk_2d.so:system/lib/libnvddk_2d.so \
   vendor/htc/endeavoru/proprietary/lib/libnvddk_2d_v2.so:system/lib/libnvddk_2d_v2.so \
   vendor/htc/endeavoru/proprietary/lib/libnvglsi.so:system/lib/libnvglsi.so \
@@ -90,9 +98,6 @@ PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/lib/libnvomx.so:system/lib/libnvomx.so \
   vendor/htc/endeavoru/proprietary/lib/libnvos.so:system/lib/libnvos.so \
   vendor/htc/endeavoru/proprietary/lib/libnvparser.so:system/lib/libnvparser.so \
-  vendor/htc/endeavoru/proprietary/lib/libnvremoteevtmgr.so:system/lib/libnvremoteevtmgr.so \
-  vendor/htc/endeavoru/proprietary/lib/libnvremotell.so:system/lib/libnvremotell.so \
-  vendor/htc/endeavoru/proprietary/lib/libnvremoteprotocol.so:system/lib/libnvremoteprotocol.so \
   vendor/htc/endeavoru/proprietary/lib/libnvrm_graphics.so:system/lib/libnvrm_graphics.so \
   vendor/htc/endeavoru/proprietary/lib/libnvrm.so:system/lib/libnvrm.so \
   vendor/htc/endeavoru/proprietary/lib/libnvsm.so:system/lib/libnvsm.so \
@@ -123,6 +128,7 @@ PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin:system/etc/firmware/ti-connectivity/wl128x-fw-4-mr.bin \
   vendor/htc/endeavoru/proprietary/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin:system/etc/firmware/ti-connectivity/wl128x-fw-4-plt.bin \
   vendor/htc/endeavoru/proprietary/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin:system/etc/firmware/ti-connectivity/wl128x-fw-4-sr.bin \
+  vendor/htc/endeavoru/proprietary/etc/wifi/TQS_D_1.7.ini:system/etc/wifi/TQS_D_1.7.ini \
   vendor/htc/endeavoru/proprietary/etc/AIC3008_REG_DualMic.csv:system/etc/AIC3008_REG_DualMic.csv \
   vendor/htc/endeavoru/proprietary/etc/AIC3008_REG_DualMic_XC.csv:system/etc/AIC3008_REG_DualMic_XC.csv \
   vendor/htc/endeavoru/proprietary/etc/DSP_number.txt:system/etc/DSP_number.txt \
@@ -164,11 +170,13 @@ PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/etc/soundimage/srsfx_trumedia_voice.cfg:system/etc/soundimage/srsfx_trumedia_voice.cfg \
   vendor/htc/endeavoru/proprietary/etc/soundimage/srs_geq10.cfg:system/etc/soundimage/srs_geq10.cfg \
   vendor/htc/endeavoru/proprietary/etc/soundimage/srs_global.cfg:system/etc/soundimage/srs_global.cfg \
+  vendor/htc/endeavoru/proprietary/lib/hw/audio_policy.tegra.so:system/lib/hw/audio_policy.tegra.so \
   vendor/htc/endeavoru/proprietary/lib/hw/audio.primary.tegra.so:system/lib/hw/audio.primary.tegra.so \
+  vendor/htc/endeavoru/proprietary/lib/libcall_volume.so:system/lib/libcall_volume.so \
   vendor/htc/endeavoru/proprietary/lib/libaudioavp.so:system/lib/libaudioavp.so \
   vendor/htc/endeavoru/proprietary/lib/libnvaudioservice.so:system/lib/libnvaudioservice.so \
-  vendor/htc/endeavoru/proprietary/lib/libasound.so:system/lib/libasound.so \
-  vendor/htc/endeavoru/proprietary/lib/libcall_volume.so:system/lib/libcall_volume.so \
+  vendor/htc/endeavoru/proprietary/lib/libsrsprocessing.so:system/lib/libsrsprocessing.so \
+  vendor/htc/endeavoru/proprietary/lib/soundfx/libsrsfx.so:system/lib/soundfx/libsrsfx.so \
   vendor/htc/endeavoru/proprietary/app/PGPSDownloader.apk:system/app/PGPSDownloader.apk \
   vendor/htc/endeavoru/proprietary/bin/cplc_main:system/bin/cplc_main \
   vendor/htc/endeavoru/proprietary/bin/navl_server:system/bin/navl_server \
@@ -191,16 +199,23 @@ PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/lib/libgpsservices.so:system/lib/libgpsservices.so \
   vendor/htc/endeavoru/proprietary/lib/librilpos.so:system/lib/librilpos.so \
   vendor/htc/endeavoru/proprietary/lib/libsupllocationprovider.so:system/lib/libsupllocationprovider.so \
+  vendor/htc/endeavoru/proprietary/lib/libpos.so:system/lib/libpos.so \
   vendor/htc/endeavoru/proprietary/lib/libmcphal.so:system/lib/libmcphal.so \
   vendor/htc/endeavoru/proprietary/lib/hw/gralloc.tegra.so:system/lib/hw/gralloc.tegra.so \
   vendor/htc/endeavoru/proprietary/lib/hw/hwcomposer.tegra.so:system/lib/hw/hwcomposer.tegra.so \
   vendor/htc/endeavoru/proprietary/bin/htcbatt:system/bin/htcbatt \
+  vendor/htc/endeavoru/proprietary/etc/hldm.bin:system/etc/hldm.bin \
+  vendor/htc/endeavoru/proprietary/etc/hltof.bin:system/etc/hltof.bin \
+  vendor/htc/endeavoru/proprietary/etc/hltrd.bin:system/etc/hltrd.bin \
   vendor/htc/endeavoru/proprietary/bin/ewtzmud:system/bin/ewtzmud \
   vendor/htc/endeavoru/proprietary/bin/akmd:system/bin/akmd \
   vendor/htc/endeavoru/proprietary/lib/hw/sensors.endeavoru.so:system/lib/hw/sensors.endeavoru.so \
   vendor/htc/endeavoru/proprietary/lib/libsensors_mpl.so:system/lib/libsensors_mpl.so \
   vendor/htc/endeavoru/proprietary/lib/libmpl.so:system/lib/libmpl.so \
   vendor/htc/endeavoru/proprietary/lib/libmlplatform.so:system/lib/libmlplatform.so \
+  vendor/htc/endeavoru/proprietary/lib/libakmd.so:system/lib/libakmd.so \
+  vendor/htc/endeavoru/proprietary/lib/libinvensense_hal.so:system/lib/libinvensense_hal.so \
+  vendor/htc/endeavoru/proprietary/lib/libewtzmu2cali.so:system/lib/libewtzmu2cali.so \
   vendor/htc/endeavoru/proprietary/lib/egl/libEGL_perfhud.so:system/lib/egl/libEGL_perfhud.so \
   vendor/htc/endeavoru/proprietary/lib/egl/libEGL_tegra_impl.so:system/lib/egl/libEGL_tegra_impl.so \
   vendor/htc/endeavoru/proprietary/lib/egl/libEGL_tegra.so:system/lib/egl/libEGL_tegra.so \
@@ -213,11 +228,12 @@ PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/lib/hw/nfc.endeavoru.so:system/lib/hw/nfc.endeavoru.so \
   vendor/htc/endeavoru/proprietary/vendor/firmware/libpn544_fw.so:system/vendor/firmware/libpn544_fw.so \
   vendor/htc/endeavoru/proprietary/lib/libdrmdecrypt.so:system/lib/libdrmdecrypt.so \
+  vendor/htc/endeavoru/proprietary/lib/libWVphoneAPI.so:system/lib/libWVphoneAPI.so \
   vendor/htc/endeavoru/proprietary/vendor/lib/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
   vendor/htc/endeavoru/proprietary/vendor/lib/libwvm.so:system/vendor/lib/libwvm.so \
   vendor/htc/endeavoru/proprietary/vendor/lib/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
   vendor/htc/endeavoru/proprietary/vendor/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-  vendor/htc/endeavoru/proprietary/lib/hw/vendor-camera.tegra.so:system/lib/hw/vendor-camera.tegra.so \
+  vendor/htc/endeavoru/proprietary/lib/hw/camera.tegra.so:system/lib/hw/vendor-camera.tegra.so \
   vendor/htc/endeavoru/proprietary/lib/libBeautyChat.so:system/lib/libBeautyChat.so \
   vendor/htc/endeavoru/proprietary/lib/libcamerapp.so:system/lib/libcamerapp.so \
   vendor/htc/endeavoru/proprietary/lib/libcameraasd.so:system/lib/libcameraasd.so \
@@ -227,4 +243,17 @@ PRODUCT_COPY_FILES += \
   vendor/htc/endeavoru/proprietary/lib/libposteffect.so:system/lib/libposteffect.so \
   vendor/htc/endeavoru/proprietary/lib/libOlaEngineNew.so:system/lib/libOlaEngineNew.so \
   vendor/htc/endeavoru/proprietary/lib/libhtc_dis.so:system/lib/libhtc_dis.so \
-  vendor/htc/endeavoru/proprietary/etc/Flash_Loader.conf:system/etc/Flash_Loader.conf
+  vendor/htc/endeavoru/proprietary/media/GPU/ContrastCurveSet.bmp:system/media/GPU/ContrastCurveSet.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/CurveCold.bmp:system/media/GPU/CurveCold.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/CurveCross.bmp:system/media/GPU/CurveCross.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/CurveSepia.bmp:system/media/GPU/CurveSepia.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/CurveWarm.bmp:system/media/GPU/CurveWarm.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/dot_mask_0.bmp:system/media/GPU/dot_mask_0.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/dot_mask_1.bmp:system/media/GPU/dot_mask_1.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/dot_mask_2.bmp:system/media/GPU/dot_mask_2.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/dot_mask_3.bmp:system/media/GPU/dot_mask_3.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/mask_vignette_legacy.bmp:system/media/GPU/mask_vignette_legacy.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/mask_vignette_wide.bmp:system/media/GPU/mask_vignette_wide.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/stylizeColorRamp.bmp:system/media/GPU/stylizeColorRamp.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/stylizeContrastRamp.bmp:system/media/GPU/stylizeContrastRamp.bmp \
+  vendor/htc/endeavoru/proprietary/media/GPU/twoToneMaskH.bmp:system/media/GPU/twoToneMaskH.bmp
