a15082c0dc5857c22f0bd5d7645ab27fa69ca278
Ethan Chen <intervigil@gmail.com>
2014-01-16 15:45:15 -0800

    m7-common: Update media libraries
    * Package version: Oppo N1_12_A.02_131116_svn3574

--- m7-common/m7-common-vendor-blobs.mk
+++ m7-common/m7-common-vendor-blobs.mk
@@ -172,6 +172,7 @@ PRODUCT_COPY_FILES += \
         vendor/htc/m7-common/proprietary/bin/cir_fw_update:/system/bin/cir_fw_update \
         vendor/htc/m7-common/proprietary/etc/cir.img:/system/etc/cir.img \
         vendor/htc/m7-common/proprietary/lib/hw/consumerir.default.so:/system/lib/hw/consumerir.default.so \
+        vendor/htc/m7-common/proprietary/lib/libExtendedExtractor.so:/system/lib/libExtendedExtractor.so \
         vendor/htc/m7-common/proprietary/lib/libI420colorconvert.so:/system/lib/libI420colorconvert.so \
         vendor/htc/m7-common/proprietary/lib/libmm-color-convertor.so:/system/lib/libmm-color-convertor.so \
         vendor/htc/m7-common/proprietary/etc/firmware/hcheck.b00:/system/etc/firmware/hcheck.b00 \
