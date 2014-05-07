a27dc257e8db238d4be28cee9117066a5249b5b8
Jake Weinstein <xboxlover360@gmail.com>
2014-05-07 16:52:30 -0500

    m8: add HTC Power and Performance Manager (PnPMgr)

--- m8/m8-vendor-blobs.mk
+++ m8/m8-vendor-blobs.mk
@@ -24,6 +24,7 @@ PRODUCT_COPY_FILES += \
         vendor/htc/m8/proprietary/bin/mm-qcamera-app:/system/bin/mm-qcamera-app \
         vendor/htc/m8/proprietary/bin/mm-qcamera-daemon:/system/bin/mm-qcamera-daemon \
         vendor/htc/m8/proprietary/bin/mpdecision:/system/bin/mpdecision \
+	vendor/htc/m8/proprietary/bin/pnpmgr:/system/bin/pnpmgr \
         vendor/htc/m8/proprietary/bin/ptt_socket_app:/system/bin/ptt_socket_app \
         vendor/htc/m8/proprietary/bin/qseecomd:/system/bin/qseecomd \
         vendor/htc/m8/proprietary/bin/rmt_storage:/system/bin/rmt_storage \
