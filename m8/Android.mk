f71189c36ca1bae5b94dd8b7c43a328341d51304
invisiblek <dan.pasanen@gmail.com>
2014-05-12 17:47:41 -0500

    m8: add EasyAccessService
    Change-Id: I9a49b16067d974d0f9d9d17706c390ac8c5f0327

--- m8/Android.mk
+++ m8/Android.mk
@@ -27,5 +27,15 @@ LOCAL_MODULE_SUFFIX := .so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := EasyAccessService
+LOCAL_MODULE_OWNER := htc
+LOCAL_SRC_FILES := proprietary/app/EasyAccessService.apk
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .apk
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := platform
+include $(BUILD_PREBUILT)
+
 endif
 
