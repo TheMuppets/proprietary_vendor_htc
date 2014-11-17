245c913b3dcd772cb370a70af07226d5bb8da52d
Ethan Chen <intervigil@gmail.com>
2014-11-16 19:57:54 -0600

    t6-common: Update Adreno libraries
    * Package version: razor-lrx21p
    
    Change-Id: If60a2530f54a0a286be6e3a58f1923e11357fd41

--- t6-common/t6-common-vendor-blobs.mk
+++ t6-common/t6-common-vendor-blobs.mk
@@ -162,19 +162,20 @@ PRODUCT_COPY_FILES += \
         vendor/htc/t6-common/proprietary/vendor/lib/libvcsFPClient.so:/system/vendor/lib/libvcsFPClient.so \
         vendor/htc/t6-common/proprietary/lib/libgeofence.so:/system/lib/libgeofence.so \
         vendor/htc/t6-common/proprietary/lib/libloc_api_v02.so:/system/lib/libloc_api_v02.so \
-        vendor/htc/t6-common/proprietary/lib/libCB.so:/system/lib/libCB.so \
         vendor/htc/t6-common/proprietary/vendor/lib/egl/eglsubAndroid.so:/system/vendor/lib/egl/eglsubAndroid.so \
         vendor/htc/t6-common/proprietary/vendor/lib/egl/libEGL_adreno.so:/system/vendor/lib/egl/libEGL_adreno.so \
         vendor/htc/t6-common/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:/system/vendor/lib/egl/libGLESv1_CM_adreno.so \
         vendor/htc/t6-common/proprietary/vendor/lib/egl/libGLESv2_adreno.so:/system/vendor/lib/egl/libGLESv2_adreno.so \
-        vendor/htc/t6-common/proprietary/vendor/lib/egl/libGLESv2S3D_adreno.so:/system/vendor/lib/egl/libGLESv2S3D_adreno.so \
+        vendor/htc/t6-common/proprietary/vendor/lib/egl/libplayback_adreno.so:/system/vendor/lib/egl/libplayback_adreno.so \
         vendor/htc/t6-common/proprietary/vendor/lib/egl/libq3dtools_adreno.so:/system/vendor/lib/egl/libq3dtools_adreno.so \
         vendor/htc/t6-common/proprietary/vendor/lib/libadreno_utils.so:/system/vendor/lib/libadreno_utils.so \
+        vendor/htc/t6-common/proprietary/vendor/lib/libbccQTI.so:/system/vendor/lib/libbccQTI.so \
+        vendor/htc/t6-common/proprietary/vendor/lib/libC2D2.so:/system/vendor/lib/libC2D2.so \
         vendor/htc/t6-common/proprietary/vendor/lib/libc2d30-a3xx.so:/system/vendor/lib/libc2d30-a3xx.so \
         vendor/htc/t6-common/proprietary/vendor/lib/libc2d30.so:/system/vendor/lib/libc2d30.so \
-        vendor/htc/t6-common/proprietary/vendor/lib/libC2D2.so:/system/vendor/lib/libC2D2.so \
+        vendor/htc/t6-common/proprietary/vendor/lib/libCB.so:/system/vendor/lib/libCB.so \
         vendor/htc/t6-common/proprietary/vendor/lib/libgsl.so:/system/vendor/lib/libgsl.so \
-        vendor/htc/t6-common/proprietary/vendor/lib/libllvm-a3xx.so:/system/vendor/lib/libllvm-a3xx.so \
+        vendor/htc/t6-common/proprietary/vendor/lib/libllvm-qcom.so:/system/vendor/lib/libllvm-qcom.so \
         vendor/htc/t6-common/proprietary/vendor/lib/librs_adreno_sha1.so:/system/vendor/lib/librs_adreno_sha1.so \
         vendor/htc/t6-common/proprietary/vendor/lib/librs_adreno.so:/system/vendor/lib/librs_adreno.so \
         vendor/htc/t6-common/proprietary/vendor/lib/libRSDriver_adreno.so:/system/vendor/lib/libRSDriver_adreno.so \
