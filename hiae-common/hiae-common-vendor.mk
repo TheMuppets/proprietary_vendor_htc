fa3a48a2d17dd363a63093d3cfe252f59bf518db
Ethan Chen <intervigil@gmail.com>
2016-06-11 14:14:17 -0700

    hiae: Add missing libmm-disp-apis target

--- hiae-common/hiae-common-vendor.mk
+++ hiae-common/hiae-common-vendor.mk
@@ -17,6 +17,7 @@
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \
     TimeService \
+    libmm-disp-apis \
     libril \
     libtime_genoff \
     libTimeService
