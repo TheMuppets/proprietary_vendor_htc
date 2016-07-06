7be86ea414806f1e0a38997eba701a5db7250acb
Dan Pasanen <invisiblek@cyanogenmod.org>
2016-07-06 09:43:31 -0500

    Revert "m8-common: update adreno from adreno_nexus_may2016.zip"
    This reverts commit 77591214c2d7ca5ee02236abc7c35c21c898ad6a.

--- m8-common/m8-common-vendor-blobs.mk
+++ m8-common/m8-common-vendor-blobs.mk
@@ -224,15 +224,12 @@ PRODUCT_COPY_FILES += \
         vendor/htc/m8-common/proprietary/etc/firmware/widevine.b02:/system/etc/firmware/widevine.b02 \
         vendor/htc/m8-common/proprietary/etc/firmware/widevine.b03:/system/etc/firmware/widevine.b03 \
         vendor/htc/m8-common/proprietary/etc/firmware/widevine.mdt:/system/etc/firmware/widevine.mdt \
-        vendor/htc/m8-common/proprietary/vendor/firmware/a330_pfp.fw:/system/vendor/firmware/a330_pfp.fw \
-        vendor/htc/m8-common/proprietary/vendor/firmware/a330_pm4.fw:/system/vendor/firmware/a330_pm4.fw \
         vendor/htc/m8-common/proprietary/vendor/lib/egl/eglsubAndroid.so:/system/vendor/lib/egl/eglsubAndroid.so \
         vendor/htc/m8-common/proprietary/vendor/lib/egl/libEGL_adreno.so:/system/vendor/lib/egl/libEGL_adreno.so \
         vendor/htc/m8-common/proprietary/vendor/lib/egl/libGLESv1_CM_adreno.so:/system/vendor/lib/egl/libGLESv1_CM_adreno.so \
         vendor/htc/m8-common/proprietary/vendor/lib/egl/libGLESv2_adreno.so:/system/vendor/lib/egl/libGLESv2_adreno.so \
         vendor/htc/m8-common/proprietary/vendor/lib/egl/libq3dtools_adreno.so:/system/vendor/lib/egl/libq3dtools_adreno.so \
         vendor/htc/m8-common/proprietary/vendor/lib/libadreno_utils.so:/system/vendor/lib/libadreno_utils.so \
-        vendor/htc/m8-common/proprietary/vendor/lib/libbccQTI.so:system/vendor/lib/libbccQTI.so \
         vendor/htc/m8-common/proprietary/vendor/lib/libC2D2.so:/system/vendor/lib/libC2D2.so \
         vendor/htc/m8-common/proprietary/vendor/lib/libc2d30-a3xx.so:/system/vendor/lib/libc2d30-a3xx.so \
         vendor/htc/m8-common/proprietary/vendor/lib/libc2d30.so:/system/vendor/lib/libc2d30.so \
@@ -245,6 +242,13 @@ PRODUCT_COPY_FILES += \
         vendor/htc/m8-common/proprietary/vendor/lib/librs_adreno.so:/system/vendor/lib/librs_adreno.so \
         vendor/htc/m8-common/proprietary/vendor/lib/libRSDriver_adreno.so:/system/vendor/lib/libRSDriver_adreno.so \
         vendor/htc/m8-common/proprietary/vendor/lib/libsc-a3xx.so:/system/vendor/lib/libsc-a3xx.so \
+        vendor/htc/m8-common/proprietary/etc/firmware/a225p5_pm4.fw:/system/etc/firmware/a225p5_pm4.fw \
+        vendor/htc/m8-common/proprietary/etc/firmware/a225_pfp.fw:/system/etc/firmware/a225_pfp.fw \
+        vendor/htc/m8-common/proprietary/etc/firmware/a225_pm4.fw:/system/etc/firmware/a225_pm4.fw \
+        vendor/htc/m8-common/proprietary/etc/firmware/a300_pfp.fw:/system/etc/firmware/a300_pfp.fw \
+        vendor/htc/m8-common/proprietary/etc/firmware/a300_pm4.fw:/system/etc/firmware/a300_pm4.fw \
+        vendor/htc/m8-common/proprietary/etc/firmware/a330_pfp.fw:/system/etc/firmware/a330_pfp.fw \
+        vendor/htc/m8-common/proprietary/etc/firmware/a330_pm4.fw:/system/etc/firmware/a330_pm4.fw \
         vendor/htc/m8-common/proprietary/etc/firmware/leia_pfp_470.fw:/system/etc/firmware/leia_pfp_470.fw \
         vendor/htc/m8-common/proprietary/etc/firmware/leia_pm4_470.fw:/system/etc/firmware/leia_pm4_470.fw \
         vendor/htc/m8-common/proprietary/bin/ATFWD-daemon:/system/bin/ATFWD-daemon \
