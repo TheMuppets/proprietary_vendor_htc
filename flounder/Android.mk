90d998af7f0f2458f34b04183e338772f42ff604
Bruno Martins <bgcngm@gmail.com>
2017-11-18 15:31:31 +0000

    flounder-common: Transform into common tree
    Change-Id: Ie38cb4d564d228626596232b905bb898445e071e

--- flounder/Android.mk
+++ flounder/Android.mk
@@ -19,16 +19,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),flounder)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := HotwordEnrollment
-LOCAL_MODULE_OWNER := htc
-LOCAL_SRC_FILES := proprietary/priv-app/HotwordEnrollment/HotwordEnrollment.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
 endif
