cd8388e0ac88630b6caef9704b61fa972cfbfc40
Bruno Martins <bgcngm@gmail.com>
2018-02-06 21:00:33 +0000

    Revert "m8-common: Include newest FM HAL dependency blobs"
    The dependency has been removed.
    
    This reverts commit fc395f778d39bddf2197dcdf948f324a645ff8ab.
    
    Change-Id: I2d0b5c9456ede0b2e76bfe464df69c460f9310d3

--- m8-common/Android.mk
+++ m8-common/Android.mk
@@ -20,16 +20,6 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter m8 m8d,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := vendor.qti.hardware.fm@1.0
-LOCAL_MODULE_OWNER := htc
-LOCAL_SRC_FILES := proprietary/lib/vendor.qti.hardware.fm@1.0.so
-LOCAL_MULTILIB := 32
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := libtime_genoff
 LOCAL_MODULE_OWNER := htc
 LOCAL_SRC_FILES := proprietary/vendor/lib/libtime_genoff.so
